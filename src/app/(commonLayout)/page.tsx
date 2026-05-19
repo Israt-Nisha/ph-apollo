718bfa3d193512d7173602d2b9df8fce295e83cf
Israt-Nisha <isratnisha1999@gmail.com>
2026-05-19 21:01:54 +0600

    after part-15 home page add

---
+++ src/app/(commonLayout)/page.tsx
@@ -0,0 +1,21 @@
+import { Button } from "@/components/ui/button";
+
+import PublicNavbar from "@/components/modules/home/PublicNavbar";
+import Hero from "@/components/modules/home/Hero";
+import Steps from "@/components/modules/home/Steps";
+import TopDoctors from "@/components/modules/home/TopDoctors";
+import PublicFooter from "@/components/modules/home/PublicFooter";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <PublicNavbar />
+      <main className="flex-grow">
+        <Hero />
+        <Steps />
+        <TopDoctors />
+      </main>
+      <PublicFooter />
+    </div>
+  );
+}
